b394fc58c1b829eb714d47cfba3cb9fad9d02fe9
João Gabriel Nascimento da Silva <75106078+Jaum18@users.noreply.github.com>
2021-07-09 08:39:36 -0300

    design patterns 1

---
+++ observadores.py
@@ -0,0 +1,12 @@
+
+
+def imprime(nota_fiscal):
+    print('Imprimindo nota fiscal {}'.format(nota_fiscal.cnpj))
+
+
+def envia_por_email(nota_fiscal):
+    print('Enviando nota fiscal por e-mail {}'.format(nota_fiscal.cnpj))
+
+
+def salva_no_banco(nota_fiscal):
+    print('Salvando no banco {}'.format(nota_fiscal.cnpj))
